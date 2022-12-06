6c0cece52c9d1138998b742e6970e82f974b0749
EhsonjonKomiljonov <pavelklivinskij@gmail.com>
2022-12-06 15:10:01 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,140 @@
+// var elTItle = document.querySelector(".site-title");
+// var elForm = document.querySelector(".form");
+// var elInput1 = document.querySelector(".form__input");
+// var elInput2 = document.querySelector(".form__input2");
+// var elBtn1 = document.querySelector(".btn1");
+// var elBtn2 = document.querySelector(".btn2");
+// var elResult = document.querySelector(".result");
+
+// var array = [
+//   "Bugatti ",
+//   "Corvette ",
+//   "BMW ",
+//   "Lamborghini ",
+//   "Mercedes ",
+//   "Toyota",
+// ];
+
+// elForm.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+//   // var array = [
+//   //   "Bugatti ",
+//   //   "Corvette ",
+//   //   "BMW ",
+//   //   "Lamborghini ",
+//   //   "Mercedes ",
+//   //   "Toyota",
+//   // ];
+
+//   elBtn1.addEventListener("click", function () {
+//       elInput1Value = +elInput1.value;
+//       elTItle.textContent = array.unshift(elInput1Value);
+//   });
+
+//   elBtn2.addEventListener("click", function () {
+//       elInput2Value = +elInput2.value;
+//       elTItle.textContent = array.push(elInput2Value);
+//   });
+// });
+
+// elTItle.textContent = array;
+
+// var userNumber = +prompt("Son kiriting");
+// var i = 0;
+
+// for (i;i <= userNumber; i += 2) {
+//   console.log(i);
+// }
+
+
+
+
+
+
+// ------------------------------UY SIHI---------------------
+
+// ---------------------------1-MASALA-----------------------
+
+// function addition(a, b) {
+// 	return a + b;
+// }
+
+// ---------------------------1-MASALA-----------------------
+
+// ---------------------------2-MASALA-----------------------
+
+// function convert(minutes) {
+// 	var secund = 60;
+// 	return secund * minutes;
+// }
+
+// ---------------------------2-MASALA-----------------------
+
+// ---------------------------3-MASALA-----------------------
+
+// function addition(num) {
+// 	return ++num;
+// }
+
+// ---------------------------3-MASALA-----------------------
+
+// ---------------------------4-MASALA-----------------------
+
+// function triArea(base, height) {
+// 	return base * height / 2;
+// }
+
+// ---------------------------4-MASALA-----------------------
+
+// ---------------------------5-MASALA-----------------------
+
+// function calcAge(age) {
+// 	return age * 365;
+// }
+
+// ---------------------------5-MASALA-----------------------
+
+// ---------------------------6-MASALA-----------------------
+
+// function remainder(x, y) {
+// 	return x % y;
+// }
+
+// ---------------------------6-MASALA-----------------------
+
+// ---------------------------7-MASALA-----------------------
+
+// function footballPoints(wins, draws, losses) {
+// 	return wins * 3 + draws * 1;
+// }
+
+// ---------------------------7-MASALA-----------------------
+
+
+// ---------------------------8-MASALA-----------------------
+
+// function points(twoPointers, threePointers) {
+// 	return twoPointers * 2 + threePointers * 3;
+// }
+
+// ---------------------------8-MASALA-----------------------
+
+// ---------------------------9-MASALA-----------------------
+
+// function lessThan100(a, b) {
+// 	if(a + b < 100){
+//     return true;
+//   }else{
+//     return false;
+//   }
+// }
+
+// ---------------------------9-MASALA-----------------------
+
+// ---------------------------10-MASALA-----------------------
+
+// function animals(chickens, cows, pigs) {
+// 	return chickens * 2 + cows * 4 + pigs * 4;
+// }
+
+// ---------------------------10-MASALA-----------------------
